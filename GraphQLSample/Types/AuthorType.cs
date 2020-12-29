64187b757dc2e22132b09760b2190dd10743bd3c
Srujana R <66719777+SrujanaGitHub@users.noreply.github.com>
2020-12-29 16:54:26 +0530

    Add files via upload

---
+++ GraphQLSample/Types/AuthorType.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using GraphQLSample.Models;
+
+namespace GraphQLSample.Types
+{
+    public class AuthorType : ObjectGraphType<Author>
+    {
+        public AuthorType()
+        {
+            Name = "Author";
+            Field(_ => _.Id).Description("Author's Id.");
+            Field(_ => _.FirstName).Description
+            ("First name of the author");
+            Field(_ => _.LastName).Description
+            ("Last name of the author");
+        }
+    }
+}
