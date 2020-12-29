8ea40caf50a69015489dcb77993818b13e66ff21
Srujana R <66719777+SrujanaGitHub@users.noreply.github.com>
2020-12-29 16:43:16 +0530

    Add files via upload

---
+++ GraphQLSample/Models/CreateAuthorInputType.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLSample.Models
+{
+    public class CreateAuthorInputType:InputObjectGraphType
+    {
+        public CreateAuthorInputType()
+        {
+            Name = "createAuthorInput1";
+            //Field<NonNullGraphType<IntGraphType>>("id");
+            Field<NonNullGraphType<StringGraphType>>("firstName");
+            Field<NonNullGraphType<StringGraphType>>("lastName");
+        }
+    }
+}
---
+++ GraphQLSample/Models/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL;
+using GraphQL.NewtonsoftJson;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphQLSample.Models
+{
+    public static class StringExtensions
+    {
+        public static Inputs ToInputs(this JObject obj)
+        {
+            var variables = obj?.GetValue() as Dictionary<string, object>
+                            ?? new Dictionary<string, object>();
+            return new Inputs(variables);
+        }
+    }
+}
